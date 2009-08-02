02db0d213cc3c9fc12c7eb182c3fc7bf04717608
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-08-02 13:02:49 +0900

    Improved? datamapper pagination

--- model/init.rb
+++ model/init.rb
@@ -7,11 +7,18 @@ require __DIR__('something.rb')
 DataMapper.auto_upgrade!
 
 # 
-# ramaze (2009.06.12) paginate for datamapper
+# ramaze (2009.07) paginate for datamapper
 #
+require 'dm-aggregates'
 require 'ramaze/helper/paginate'
 class DataMapper::Collection
   def paginate(page, limit)
+    def self.size
+      self.count
+    end
+    def self.empty?
+      size != 0
+    end
     Ramaze::Helper::Paginate::Paginator::ArrayPager.new(self, page, limit)
   end
 end
